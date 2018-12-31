4444bfc209c8115b89ce617e520fc78a83637dcd
umluizlima <umluizlima@gmail.com>
2018-12-31 13:02:55 -0200

    Refactor division_by_zero test to stop flake8 errors

--- test_calculator.py
+++ test_calculator.py
@@ -21,5 +21,5 @@ class TestCalculator:
         assert 2.5 == calculator.divide(5, 2)
 
     def test_division_by_zero(self):
-        with pytest.raises(ZeroDivisionError) as e_info:
-            result = calculator.divide(2, 0)
+        with pytest.raises(ZeroDivisionError):
+            calculator.divide(2, 0)
